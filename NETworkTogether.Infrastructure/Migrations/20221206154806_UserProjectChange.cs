5c4627153c425e868ccda24b6fa5a3a07d8bc05c
Mateusz Boroch <m.boroch2@gmail.com>
2022-12-06 16:49:08 +0100

    Add migration with change in UserProject class

---
+++ NETworkTogether.Infrastructure/Migrations/20221206154806_UserProjectChange.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NETworkTogether.Infrastructure.Migrations
+{
+    public partial class UserProjectChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanEdit",
+                table: "UserProject",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanEdit",
+                table: "UserProject");
+        }
+    }
+}
--- NETworkTogether.Infrastructure/Repository/UserRepository.cs
+++ NETworkTogether.Infrastructure/Repository/UserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using NETworkTogether.Domain.Interface;
+using NETworkTogether.Domain.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,29 @@ namespace NETworkTogether.Infrastructure.Repository
 {
     public class UserRepository : IUserRepository
     {
+        public void DeleteUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserData(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Post> GetUserPosts(string userPosts)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Project> GetUserProjects(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateUserData(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
